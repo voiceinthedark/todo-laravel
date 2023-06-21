a5e065543d30810cae421cadd7bd5b046d6a494f
voiceinthedark <darkrisingforce@gmail.com>
2023-06-21 18:41:27 +0300

    Added toasts

--- resources/views/livewire/todo-item.blade.php
+++ resources/views/livewire/todo-item.blade.php
@@ -8,18 +8,23 @@
             <button x-on:click="editingTodo = !editingTodo" class="text-green-500 px-4">
                 <x-icon-pencil />
             </button>
-            <button wire:click="deleteTodo" class="text-red-500">
+            <button wire:click="deleteTodo" x-on:click="$dispatch('notice', {type: 'info', text: 'deleted successfully'})" class="text-red-500">
                 <x-icon-trash />
             </button>
         </div>
     </div>
     <!-- Show this div content only when editing by pressing the pencil button -->
-    <div x-show="editingTodo"  x-on:click.away="editingTodo = false">
+    <div x-show="editingTodo"
+        x-on:click.away="editingTodo = false"
+        x-on:click.outside="$dispatch('notice', {type: 'success', text: 'Edited successfully'})"
+        x-transition.duration.500ms
+        >
+
         <input type="text"
             wire:model="todo.title"
             wire:keydown.enter="updateTodo({{ $todo->id }})"
             placeholder="{{ $todo->title }}"
-            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40">
+            class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-dashed focus:ring focus:ring-opacity-40">
 
     </div>
 </li>
